63411e84656a33c2ebbbd7f31d4b8b95352a5bbf
Sandeepan Banerjee <shondha.tara94@gmail.com>
2024-04-15 20:01:23 +0530

    Init 2 component signin and singup

--- src/app/app.module.ts
+++ src/app/app.module.ts
@@ -4,10 +4,14 @@ import { BrowserModule } from '@angular/platform-browser';
 import { AppRoutingModule } from './app-routing.module';
 import { AppComponent } from './app.component';
 import { provideAnimationsAsync } from '@angular/platform-browser/animations/async';
+import { SignupComponent } from './pages/signup/signup.component';
+import { SinginComponent } from './pages/singin/singin.component';
 
 @NgModule({
   declarations: [
-    AppComponent
+    AppComponent,
+    SignupComponent,
+    SinginComponent,
   ],
   imports: [
     BrowserModule,
